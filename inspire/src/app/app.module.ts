384f1fed17d5ac8d239f038dcf4d4a2f52a61054
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2024-04-26 17:08:40 +0200

    install primeNg succefully and testing it using a dump component Button, created in components/button

--- inspire/src/app/app.module.ts
+++ inspire/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ButtonComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule],
+  providers: [provideAnimations()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
