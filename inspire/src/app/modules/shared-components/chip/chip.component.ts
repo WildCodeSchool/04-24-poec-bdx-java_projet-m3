2727aa70d85ef8a86dd0b94c12630654d592eb76
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2024-04-27 23:49:27 +0200

    making chips and card-skill

--- inspire/src/app/modules/shared-components/chip/chip.component.ts
+++ inspire/src/app/modules/shared-components/chip/chip.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chip',
   templateUrl: './chip.component.html',
   styleUrl: './chip.component.scss',
 })
-export class ChipComponent {}
+export class ChipComponent {
+  @Input() title: string = 'JavaScript';
+}
