515cc1912471abbba0486741a01fdef269b5f479
Hepsox <marie.delaire@outlook.com>
2024-04-27 17:56:47 +0200

    validator input register

--- inspire/src/app/modules/auth/modules/register/register.module.ts
+++ inspire/src/app/modules/auth/modules/register/register.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
-
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { SharedComponentsModule } from '../../../shared-components/shared-components.module';
+import { RegisterFormComponent } from './components/feature/register-form/register-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [RegisterPageComponent, RegisterFormComponent],
   imports: [
     CommonModule,
-    RegisterRoutingModule
-  ]
+    RegisterRoutingModule,
+    SharedComponentsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class RegisterModule { }
+export class RegisterModule {}
