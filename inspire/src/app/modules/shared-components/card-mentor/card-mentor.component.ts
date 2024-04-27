4e3bbb3cc7857906d8d1739372745ef427695fe5
Mathieu Chauveau <chauveau.math@gmail.com>
2024-04-27 18:59:34 +0200

    create card-mentor

---
+++ inspire/src/app/modules/shared-components/card-mentor/card-mentor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-mentor',
+  templateUrl: './card-mentor.component.html',
+  styleUrl: './card-mentor.component.scss'
+})
+export class CardMentorComponent {
+
+}
--- inspire/src/app/modules/shared-components/shared-components.module.ts
+++ inspire/src/app/modules/shared-components/shared-components.module.ts
@@ -4,9 +4,10 @@ import { ButtonComponent } from './button/button.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SocialConnexionComponent } from './social-connexion/social-connexion.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { CardMentorComponent } from './card-mentor/card-mentor.component';
 
 @NgModule({
-  declarations: [ButtonComponent, WelcomeComponent, SocialConnexionComponent, SideNavComponent],
+  declarations: [ButtonComponent, WelcomeComponent, SocialConnexionComponent, SideNavComponent, CardMentorComponent],
   imports: [CommonModule],
   exports: [ButtonComponent, WelcomeComponent, SocialConnexionComponent],
 })
