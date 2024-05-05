347e9b2185c4338cb5ba73ec1f3136fefd12f81e
Hepsox <marie.delaire@outlook.com>
2024-05-05 17:58:37 +0200

    Update user store service to store user object in local storage after login

--- inspire/src/app/app.component.ts
+++ inspire/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserStoreService } from './shared/services/stores/user-store.service';
+import { User } from './shared/models/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'inspire';
+export class AppComponent implements OnInit, OnDestroy {
+  userSubscription: Subscription | undefined;
+  userDansLeLocalStorage: User | null = null;
+
+  constructor(private store: UserStoreService) {}
+
+  ngOnInit(): void {
+    this.userSubscription = this.store.getUserConnected$().subscribe((user) => {
+      this.userDansLeLocalStorage = user;
+      console.log(this.userDansLeLocalStorage);
+    });
+
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const user: User = JSON.parse(storedUser);
+      this.store.setUserConnected(user);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
 }
