365a3349f2f9567b88074c1c2783f3415ccb582f
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2024-05-08 13:50:06 +0200

    modal edition language : modal reactive en fonction des languages fetchés

--- inspire/src/app/modules/shared-components/modal-edit-languages/modal-edit-languages.component.ts
+++ inspire/src/app/modules/shared-components/modal-edit-languages/modal-edit-languages.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Language } from '../../../shared/models/language';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-edit-languages',
@@ -13,52 +12,41 @@ export class ModalEditLanguagesComponent implements OnInit {
   @Output() onCancel = new EventEmitter();
   @Input() visible: boolean = true;
 
-  languages!: Language[];
   @Input() selectedLanguages!: Language[];
+  @Input() languages: Language[] = [
+    {
+      id: '1457',
+      name: 'francais',
+    },
+    {
+      id: '9b4b',
+      name: 'anglais',
+    },
+    {
+      id: 'c082',
+      name: 'arabe',
+    },
+    {
+      id: '90b9',
+      name: 'italien',
+    },
+    {
+      id: '23a9',
+      name: 'espagnol',
+    },
+    {
+      id: '07ed',
+      name: 'chinois',
+    },
+  ];
 
   focusBtnCancel = true;
   focusBtnValidate = false;
 
-  formGroup!: FormGroup;
-
   constructor() {}
 
   ngOnInit(): void {
-    this.languages = [
-      {
-        name: 'arabe',
-      },
-      {
-        name: 'français',
-      },
-      {
-        name: 'anglais',
-      },
-      {
-        name: 'espagnol',
-      },
-      {
-        name: 'portugais',
-      },
-      {
-        name: 'chinois',
-      },
-      {
-        name: 'japonais',
-      },
-      {
-        name: 'hebreux',
-      },
-      {
-        name: 'français',
-      },
-    ];
-
-    this.formGroup = new FormGroup({
-      selectedLanguages: new FormControl<Language[] | null>(
-        this.selectedLanguages
-      ),
-    });
+    console.log(this.selectedLanguages);
   }
 
   showDialog() {
