c7d4359f95adf015587fe29a018eed65dded0011
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2024-05-12 00:05:08 +0200

    adding some settings

--- inspire/src/app/modules/mentor/components/features/calendar/calendar.component.ts
+++ inspire/src/app/modules/mentor/components/features/calendar/calendar.component.ts
@@ -1,30 +1,70 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import { FullCalendarComponent } from '@fullcalendar/angular';
+import { WeekDay } from '@angular/common';
+import frLocale from '@fullcalendar/core/locales/fr';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
 })
-export class CalendarComponent implements AfterViewInit {
+export class CalendarComponent implements OnInit, AfterViewChecked {
   @ViewChild('calendar') calendarComponent!: FullCalendarComponent;
   today!: string;
 
   calendarOptions: CalendarOptions = {
     initialView: 'timeGridWeek',
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
-    locale: 'fr',
+
+    locale: frLocale,
     headerToolbar: {
-      right: 'prev, next, today',
+      right: 'today prev,next',
       left: 'dayGridMonth timeGridWeek timeGridDay',
     },
+    views: {
+      dayGridMonth: {
+        titleFormat: { year: 'numeric', month: '2-digit', day: '2-digit' },
+      },
+      timeGridFiveDays: {
+        type: 'timeGrid',
+        duration: { days: 4 },
+      },
+    },
+    buttonText: {
+      today: "Aujourd'hui",
+      month: 'Mois',
+      week: 'Semaine',
+      day: 'Jour',
+      list: 'list',
+      allDayText: 'tous',
+    },
+    weekends: false,
+    slotDuration: '00:15:00',
+    slotMinTime: '06:00',
+    slotMaxTime: '19:00',
+
+    navLinks: true,
+
+    weekNumbers: true,
   };
 
-  ngAfterViewInit(): void {
-    this.today = this.calendarComponent.getApi().view.title;
+  ngOnInit(): void {
+    this.calendarOptions.locale = frLocale;
+    this.calendarOptions.allDayText = 'Heures';
+  }
+  ngAfterViewChecked(): void {
+    setTimeout(() => {
+      this.today = this.calendarComponent.getApi().view.title;
+    }, 0);
   }
 }
