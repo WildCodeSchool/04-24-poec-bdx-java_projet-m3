7c955275d025d4712e1bb7061fe6332e7d7811e4
Hepsox <marie.delaire@outlook.com>
2024-05-26 15:19:03 +0200

    select option visio or presentiel

--- inspire/src/app/modules/mentor/components/features/calendar/calendar.component.ts
+++ inspire/src/app/modules/mentor/components/features/calendar/calendar.component.ts
@@ -16,7 +16,7 @@ import { ReservationService } from '../../../../../shared/services/reservation.s
 import { MentorService } from '../../../../../shared/services/mentor.service';
 import { Subscription } from 'rxjs';
 import { Mentor } from '../../../../../shared/models/user';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-calendar',
@@ -37,12 +37,21 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
   events: EventInput[] = [];
   displayModal: boolean = false;
   eventDetails: any = {};
+  mode: string = '';
 
   constructor(
     private reservationService: ReservationService,
     private mentorService: MentorService,
     private fb: FormBuilder
   ) {}
+
+  formulaire: FormGroup = this.fb.group({
+    mode: ['presentiel'],
+  });
+
+  updateMode() {
+    this.formattedSlotInfo.visio = this.formulaire.value.mode === 'visio';
+  }
 
   selectAllow = (selectionInfo: any) => {
     if (selectionInfo.start > new Date()) {
@@ -52,15 +61,20 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
   };
 
   onDateSelect = (selectionInfo: any) => {
-    this.formattedSlotInfo = {
-      dateTime: selectionInfo.startStr,
-      dateEnd: selectionInfo.endStr,
-      visio: true,
-      mentorId: this.mentorId,
-    };
+    if (this.formulaire.valid) {
+      this.formattedSlotInfo = {
+        dateTime: selectionInfo.startStr,
+        dateEnd: selectionInfo.endStr,
+        visio: this.formulaire.value.mode === 'visio',
+        mentorId: this.mentorId,
+      };
 
-    this.visible = true;
+      this.visible = true;
+    } else {
+      console.log("Veuillez d'abord soumettre le formulaire.");
+    }
   };
+
   validateSlot() {
     this.reservationService
       .addSlotToMentor(this.formattedSlotInfo)
