fb3a56d301a4adbaa57cdc2c826a88fcbafd9378
Hepsox <marie.delaire@outlook.com>
2024-06-13 14:14:20 +0200

    chore: Update login form component

--- inspire/src/app/modules/auth/modules/login/components/feature/login-form/login-form.component.ts
+++ inspire/src/app/modules/auth/modules/login/components/feature/login-form/login-form.component.ts
@@ -1,32 +1,23 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from '../../../../../../../user.service';
-import { emailPasswordValidator } from '../../../validator-connexion/validator-connexion';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss'],
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
-  loginForm = this.fb.group(
-    {
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-    },
-    { validators: emailPasswordValidator('email', 'password') }
-  );
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
   modalVisible = false;
-
   constructor(private fb: FormBuilder, private userService: UserService) {}
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-      this.userService.login(email || '', password || '').subscribe();
-    } else {
-      console.log('Form is invalid', this.loginForm.errors);
-    }
+    const { email, password } = this.loginForm.value;
+    this.userService.login(email || '', password || '').subscribe();
   }
 }
